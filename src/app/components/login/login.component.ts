8ea314da5ce1179b5d92c02590fd3e6b8a8062f4
fcesc-code <bpfran@gmail.com>
2021-09-26 16:10:36 +0200

    feature / implemented userDTO in LoginComponent

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { userDTO } from 'src/app/models/user.dto';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.sass']
 })
 export class LoginComponent implements OnInit {
+  user: userDTO;
 
-  constructor() { }
+  constructor() { 
+    this.user = new userDTO('', '');
+  }
 
   ngOnInit(): void {
+  }
+
+  checkLogin(): void {
+    console.log(`User data -> Email: ${this.user.email}, Password: ${this.user.password}`);
   }
 
 }
