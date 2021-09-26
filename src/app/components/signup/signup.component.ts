f5145164fe6db15cfc50af3eb6ae45d6c3e81708
fcesc-code <bpfran@gmail.com>
2021-09-26 20:22:39 +0200

    feature / added validation in signup form, improved feedback to user

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -10,19 +10,30 @@ import { userNew } from 'src/app/models/user.new';
 export class SignupComponent {
   user: userNew;
   isValidForm: boolean;
+  equalPasswords: boolean;
 
   constructor() { 
     this.user = new userNew();
     this.isValidForm = false;
+    this.equalPasswords = false;
   }
 
   checkSignup(form: NgForm): void {
-    this.isValidForm = Boolean(form.valid);
+    this.equalPasswords = (form.value.password === form.value.confirmPassword);
+    this.isValidForm = (form.valid) ? true : false;
+
     this.devPrintUserData(this.user);
   }
 
   private devPrintUserData(someUser: userNew): void {
     let userData = 'User data for dev purposes: \n';
-    console.table(someUser);
+    let formError = 'Invalid form';
+    let differentPasswordsError = 'Passwords do not match';
+
+    console.log(userData);
+    if (!this.equalPasswords) console.error(differentPasswordsError);
+    this.isValidForm 
+      ? console.table(someUser) 
+      : console.error(formError);
   }
 }
