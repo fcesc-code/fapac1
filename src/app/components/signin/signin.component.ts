39466d78621ba33b3d0e77051b66b197ef5e00ad
fcesc-code <bpfran@gmail.com>
2021-09-30 21:49:22 +0200

    feature / created html view for form and included basic required fields

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { userDTO } from 'src/app/models/user.dto';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.sass']
 })
-export class SigninComponent implements OnInit {
+export class SigninComponent {
+  user: userDTO;
+  email: FormControl;
+  password: FormControl;
+  signinForm: FormGroup;
 
-  constructor() { }
+  constructor( private formbuilder: FormBuilder ) { 
+    this.user = new userDTO('', '');
+    this.email = new FormControl( this.user.email, Validators.required );
+    this.password = new FormControl( this.user.password, Validators.required );
+    this.signinForm = this.formbuilder.group({
+      email: this.email,
+      password: this.password
+    })
+  }
 
-  ngOnInit(): void {
+  checkSignin(){
+    this.user.email = this.email.value;
+    this.user.password = this.password.value;
   }
 
 }
